d0b54f19982ec9e61c651dad1152161f3fa89ffb
Gustavo Meira <gmeira.antonio@gmail.com>
2022-06-29 17:08:20 -0300

    feat: create a method delete to task repository contract and his implementation on prisma

--- src/repositories/ITaskRepository.ts
+++ src/repositories/ITaskRepository.ts
@@ -3,6 +3,7 @@ import { TaskEntity } from '../entities/TaskEntity';
 interface ITaskRepository {
   create(task: TaskEntity): Promise<void>;
   getAll(userId: string): Promise<TaskEntity[]>;
+  delete(id: string): Promise<void>;
 }
 
 export { ITaskRepository };
