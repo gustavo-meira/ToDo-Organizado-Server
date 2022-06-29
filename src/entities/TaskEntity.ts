df464a0421bf67dc106bcf0d7b4febf9b168e679
Gustavo Meira <gmeira.antonio@gmail.com>
2022-06-29 10:43:31 -0300

    refactor: changing the properties on task entity from private to readonly

--- src/entities/TaskEntity.ts
+++ src/entities/TaskEntity.ts
@@ -4,57 +4,29 @@ type TaskEntityProps = {
   id: string;
   title: string;
   startDate?: Date;
-  deadline?: Date;
+  deadline?: Date | null;
   status?: TaskStatus;
-  details?: string;
+  details?: string | null;
   userId: string;
 }
 
 class TaskEntity {
-  private _id: string;
-  private _title: string;
-  private _startDate: Date;
-  private _deadline: Date | null;
-  private _status: TaskStatus;
-  private _details: string | null;
-  private _userId: string;
+  readonly id: string;
+  readonly title: string;
+  readonly startDate: Date;
+  readonly deadline: Date | null;
+  readonly status: TaskStatus;
+  readonly details: string | null;
+  readonly userId: string;
 
   constructor(taskProps: TaskEntityProps) {
-    this._id = taskProps.id;
-    this._title = taskProps.title;
-    this._startDate = taskProps.startDate === undefined ? new Date() : taskProps.startDate;
-    this._status = taskProps.status === undefined ? 'pending' : taskProps.status;
-    this._userId = taskProps.userId;
-    this._deadline = taskProps.deadline === undefined ? null : taskProps.deadline;
-    this._details = taskProps.details === undefined ? null : taskProps.details;
-  }
-
-  get id(): string {
-    return this._id;
-  }
-
-  get title(): string {
-    return this._title;
-  }
-
-  get startDate(): Date {
-    return this._startDate;
-  }
-
-  get deadline(): Date | null {
-    return this._deadline;
-  }
-
-  get status(): TaskStatus {
-    return this._status;
-  }
-
-  get details(): string | null {
-    return this._details;
-  }
-
-  get userId(): string {
-    return this._userId;
+    this.id = taskProps.id;
+    this.title = taskProps.title;
+    this.startDate = taskProps.startDate === undefined ? new Date() : taskProps.startDate;
+    this.status = taskProps.status === undefined ? 'pending' : taskProps.status;
+    this.userId = taskProps.userId;
+    this.deadline = taskProps.deadline === undefined ? null : taskProps.deadline;
+    this.details = taskProps.details === undefined ? null : taskProps.details;
   }
 }
 
